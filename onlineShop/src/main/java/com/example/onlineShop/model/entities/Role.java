8a1c9ebacc93cc36f0bc24a6cb9d7db30e9d0d83
Alexapod123 <alex_krim99@mail.ru>
2025-01-03 19:57:41 +0300

    change role and user

--- onlineShop/src/main/java/com/example/onlineShop/model/entities/Role.java
+++ onlineShop/src/main/java/com/example/onlineShop/model/entities/Role.java
@@ -2,6 +2,9 @@ package com.example.onlineShop.model.entities;
 
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * Роль пользователя
+ */
 public enum Role implements GrantedAuthority {
     CLIENT, ADMIN;
 
--- onlineShop/src/main/java/com/example/onlineShop/model/entities/User.java
+++ onlineShop/src/main/java/com/example/onlineShop/model/entities/User.java
@@ -12,28 +12,54 @@ import java.util.Set;
 @Table(name = "USERS")
 @Data
 public class User {
+    /**
+     * Уникальный id пользователя
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    /**
+     * Имя пользователя
+     */
     @Column(name = "name")
     private String name;
 
+    /**
+     * Фамилия пользователя
+     */
     @Column(name = "lastname")
     private String lastname;
 
+    /**
+     * Электронная почта пользователя
+     */
     @Column(name = "email")
     private String email;
 
+    /**
+     * Пароль пользователя
+     */
     @Column(name = "password")
     private String password;
 
+    /**
+     * Почтовый адрес
+     */
     @Column(name = "address")
-    private String addressDelivery;
+    private String address;
 
+    /**
+     * Роль пользователя
+     */
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "userRole", joinColumns = @JoinColumn(name = "userId"))
     @Enumerated(EnumType.STRING)
     private Set<Role> role;
+
+    /**
+     * Конструктор без параметров
+     */
+    User(){}
 }
