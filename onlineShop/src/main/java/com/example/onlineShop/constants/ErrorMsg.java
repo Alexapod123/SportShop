657a60049c3e984b07c63eaea1f2b6d447469202
Alexapod123 <alex_krim99@mail.ru>
2025-01-07 14:03:17 +0300

    add constants

---
+++ onlineShop/src/main/java/com/example/onlineShop/constants/ErrorMsg.java
@@ -0,0 +1,15 @@
+package com.example.onlineShop.constants;
+
+public class ErrorMsg {
+    public static final String EMAIL_NOT_FOUND = "Email not found";
+    public static final String EMAIL_IS_BUSY = "Email already exists";
+    public static final String INVALID_EMAIL = "Invalid email";
+    public static final String EMPTY_EMAIL= "String 'email' is empty";
+    public static final String ORDER_NOT_FOUND = "Order not found";
+    public static final String PRODUCT_NOT_FOUND = "Product not found";
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String EMPTY_NAME = "String 'name' is empty";
+    public static final String EMPTY_LAST_NAME = "String 'lastname' is empty";
+    public static final String INVALID_PASSWORD = "Password must be between 8 and 20 characters long";
+    public static final String CAPTCHA_ERROR = "Fill captcha first";
+}
---
+++ onlineShop/src/main/java/com/example/onlineShop/constants/SuccessMsg.java
@@ -0,0 +1,8 @@
+package com.example.onlineShop.constants;
+
+public class SuccessMsg {
+    public static final String USER_UPDATED = "User updated successfully";
+    public static final String PRODUCT_ADD = "Product added successfully";
+    public static final String PRODUCT_UPDATED = "Product updated successfully";
+
+}
