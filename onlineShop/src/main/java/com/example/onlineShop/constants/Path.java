565e50fbcf1be93cc9ac42d5af86e8d45977e54d
Alexapod123 <alex_krim99@mail.ru>
2025-01-09 23:45:48 +0300

    not connection to db

--- onlineShop/src/main/java/com/example/onlineShop/constants/Path.java
+++ onlineShop/src/main/java/com/example/onlineShop/constants/Path.java
@@ -1,14 +1,14 @@
 package com.example.onlineShop.constants;
 
 public class Path {
-    public static final String PRODUCT = "/product";
-    public static final String ORDER = "/order";
-    public static final String REGISTRATION = "/registration";
-    public static final String USER = "/user";
-    public static final String ADMIN = "/admin";
-    public static final String LOGIN = "/login";
-    public static final String CART = "/cart";
-    public static final String AUTH = "/auth";
-    public static final String ERROR = "/error";
-    public static final String REVIEW = "/review";
+    public static final String PRODUCT = "/api/product";
+    public static final String ORDER = "/api/order";
+    public static final String REGISTRATION = "/api/registration";
+    public static final String USER = "/api/user";
+    public static final String ADMIN = "/api/admin";
+    public static final String LOGIN = "/api/login";
+    public static final String CART = "/api/cart";
+    public static final String AUTH = "/api/auth";
+    public static final String ERROR = "/api/error";
+    public static final String REVIEW = "/api/review";
 }
